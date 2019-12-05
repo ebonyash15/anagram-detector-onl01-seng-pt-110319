4cdebb80f2357b0526a19a02f6139b86bc3f1017
Ebony Sharrock <ebonysharrock@gmail.com>
2019-12-05 02:33:46 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    @word=word
+  end
+  def match(possible)
+    anagrams=[]
+    possible.each do |anagram|
+      if anagram.sort==@word.sort
+        anagrams<<anagram
+      end
+    end
+    anagrams
+  end
+end
+
